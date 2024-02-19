3c6d5a4daa0bb660f600c18447ae36b33177b1ba
Djimi02 <mitko.manolev1@abv.bg>
2024-02-19 20:59:08 +0100

    retrofit service added

---
+++ project/app/src/main/java/com/example/sport_app_client/retrofit/RetrofitService.java
@@ -0,0 +1,23 @@
+package com.example.sport_app_client.retrofit;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitService {
+
+    private Retrofit retrofit;
+
+    public RetrofitService() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.152:8081")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return this.retrofit;
+    }
+
+}
