8a0e9fb5964d0f828c8d544fa054732899d11928
Djimi02 <mitko.manolev1@abv.bg>
2024-04-02 21:23:55 +0200

    ;

--- project/app/src/main/java/com/example/sport_app_client/adapter/GameTeamsRVAdapter.java
+++ project/app/src/main/java/com/example/sport_app_client/adapter/GameTeamsRVAdapter.java
@@ -38,7 +38,7 @@ public class GameTeamsRVAdapter extends RecyclerView.Adapter<GameTeamsRVAdapter.
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.memberNameTV.setText("" + members.get(position).getNickname());
-        Member memberToBeDeleted = members.get(position);
+        Member<?> memberToBeDeleted = members.get(position);
         holder.removeMemberBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
